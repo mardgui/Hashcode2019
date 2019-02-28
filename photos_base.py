0908dd246690f53b555fbdcf09b008cd3b9b54cf
Quentin Guimard <quentin.guimard@etu.unice.fr>
2019-02-28 19:01:14 +0100

    List of photos in 'photos'

--- photos_base.py
+++ photos_base.py
@@ -1,7 +1,5 @@
 import main
 
-lines = main.get_non_empty_lines("assignment/a_example.txt")
-
 
 class Photo:
     def __init__(self, orientation, tags):
@@ -13,3 +11,16 @@ class Photo:
 
     def get_tags(self):
         return self.tags
+
+    def get_nb_tags(self):
+        return len(self.tags)
+
+
+lines = main.get_non_empty_lines("assignment/a_example.txt")
+photos = []
+nb_photos = int(lines[0])
+for line in lines[1:]:
+    infos = line.split(' ')
+    photos.append(Photo(infos[0], infos[2:]))
+
+print('')
